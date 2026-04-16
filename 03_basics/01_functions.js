afb97c1845f4dbd53d32d529357b5af6ed997448
MayuriSoundalkar <157484358+MayuriSoundalkar@users.noreply.github.com>
2026-04-16 07:20:57 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -15,3 +15,36 @@ function loginUser(username="Mayuri"){
 }
 
 console.log(loginUser());
+
+function calculatePrice(val1,val2,...num1){
+    return num1;
+}
+
+console.log(calculatePrice(100,200,4000,400,5000));
+
+const user  = {
+    username : "Mayuri Soundalkar",
+    id : 34,
+    course : "MCA"
+}
+
+function handleObject(anyObject){
+    return `${anyObject.username} have taken ${anyObject.course} for better career.`  
+}
+
+//console.log(handleObject(user));
+
+console.log(handleObject({
+    username : "Kiran",
+    course : "Food Technology"
+}));
+
+const newArray = [1,2,3,4];
+
+function returnSecondValue(array1){
+    return array1[1];
+}
+
+//console.log(returnSecondValue(newArray));
+console.log(returnSecondValue([3,4,5,6]));
+
